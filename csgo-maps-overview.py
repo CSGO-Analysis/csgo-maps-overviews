871cc995446a7e87c33e26807cd17c4554e25367
0BuRner <burner@live.be>
2017-04-23 09:45:08 +0200

    update maps files (auto)

--- csgo-maps-overview.py
+++ csgo-maps-overview.py
@@ -11,38 +11,50 @@ import sys
 
 # https://www.online-convert.com
 API_KEY = sys.argv[1]
-DIR = "overviews"
-URL = "http://api2.online-convert.com/jobs"
+API_URL = "http://api2.online-convert.com/jobs"
 TYPE_IN = "dds"
 TYPE_OUT = "jpg"
-headers = {
+HEADER = {
     'x-oc-api-key': API_KEY,
     'content-type': "application/json",
     'cache-control': "no-cache"
 }
+API_REQUEST_WAIT = 10
+REMOTE_URL = "http://www.tafelrunde.net/csgods/resource/overviews/"  # contains updated game files
+LOCAL_DIR = "overviews"
+FILES = [
+    'de_cache_radar',
+    'de_cache_radar_spectate',
+    'de_cbble_radar',
+    'de_dust2_radar',
+    'de_dust2_radar_spectate',
+    'de_inferno_radar',
+    'de_mirage_radar',
+    'de_mirage_radar_spectate',
+    'de_nuke_radar',
+    'de_nuke_radar_spectate',
+    'de_overpass_radar',
+    'de_train_radar',
+    'de_train_radar_spectate'
+]
 
-print API_KEY
+print "Starting..."
 
-print "Starting"
+for filename in FILES:
+    payload = "{\"input\":[{\"type\":\"remote\",\"source\":\"" + REMOTE_URL + filename + "." + TYPE_IN + "\"}],\"conversion\":[{\"category\":\"image\",\"target\":\"" + TYPE_OUT + "\"}]}"
 
-input_dir = DIR + "/"
-files = [name for name in os.listdir(input_dir) if os.path.isfile(input_dir + name) and name.endswith("." + TYPE_IN)]
-
-for filename in files:
-    payload = "{\"input\":[{\"type\":\"remote\",\"source\":\"https://github.com/CSGO-Analysis/csgo-maps-overviews/raw/master/overviews/" + filename + "\"}],\"conversion\":[{\"category\":\"image\",\"target\":\"" + TYPE_OUT + "\"}]}"
-
-    response = requests.request("POST", URL, data=payload, headers=headers)
+    response = requests.request("POST", API_URL, data=payload, headers=HEADER)
     response_json = json.loads(response.text)
     job_id = response_json['id']
 
     result = False
     while not result:
         print "Waiting",
-        for i in range(0, 10):
+        for i in range(0, API_REQUEST_WAIT):
             print ".",
             time.sleep(1)
 
-        response = requests.request("GET", URL + "/" + job_id, headers=headers)
+        response = requests.request("GET", API_URL + "/" + job_id, headers=HEADER)
         response_json = json.loads(response.text)
         if len(response.text):
             if response_json['status']['code'] == 'completed':
